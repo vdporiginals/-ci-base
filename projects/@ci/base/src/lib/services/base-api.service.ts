40e1109906b09485b9a1676f40492e47e1109290
Duy Phuong <phuong.vd@consultindochina.com>
2021-03-05 14:23:39 +0700

    change base method

--- projects/@ci/base/src/lib/services/base-api.service.ts
+++ projects/@ci/base/src/lib/services/base-api.service.ts
@@ -18,6 +18,7 @@ interface HttpOptions {
 interface Params {
   [param: string]: string | string[];
 }
+
 export abstract class BaseApiService<T> {
   private httpOptions: HttpOptions | undefined;
   constructor(
@@ -30,17 +31,18 @@ export abstract class BaseApiService<T> {
       params,
     };
     return this.http
-      .get(`${this.actionUrl}`, this.httpOptions)
-      .pipe(map((res: any) => res.Payload));
+      .get<T>(`${this.actionUrl}`, this.httpOptions)
+      .pipe(map((res: any): T[] => res.Payload));
   }
 
   detail(id: unknown, params?: Params): Observable<T> {
     this.httpOptions = {
       params,
     };
+
     return this.http
       .get<T>(`${this.actionUrl}/${id}`, this.httpOptions)
-      .pipe(map((res: any) => res.Payload));
+      .pipe(map((res: any): T => res.Payload));
   }
 
   create(
