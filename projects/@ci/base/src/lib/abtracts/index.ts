b5596ac45cf01376f63d96117e4d37259b930b26
vdp.originals <vdp.originals@gmail.com>
2021-03-06 11:46:43 +0700

    update search

--- projects/@ci/base/src/lib/abtracts/index.ts
+++ projects/@ci/base/src/lib/abtracts/index.ts
@@ -1,2 +1,3 @@
 export * from './base-api';
 export * from './list-components';
+export * from './search-input';
--- projects/@ci/base/src/public-api.ts
+++ projects/@ci/base/src/public-api.ts
@@ -1,8 +1,4 @@
 /*
  * Public API Surface of base
  */
-
-export * from './lib/base.service';
-export * from './lib/base.component';
-export * from './lib/base.module';
 export * from './lib/abtracts/index';
