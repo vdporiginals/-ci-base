e5a718541b7a77194745ed3fbe8e70a2d40f2d65
Duy Phuong <phuong.vd@consultindochina.com>
2021-04-28 09:47:09 +0700

    remove policy config

--- projects/@ci/base/src/lib/auth/config/auth.config.ts
+++ projects/@ci/base/src/lib/auth/config/auth.config.ts
@@ -1,21 +1,9 @@
 import { InjectionToken, ValueProvider } from '@angular/core';
 import { AuthConfig } from './auth-config.interface';
-import { PolicyConfig } from './policy-config.interface';
 
 export const AUTH_CONFIG = new InjectionToken<AuthConfig>('ci-auth.config');
 
 export const getAuthConfigProvider = (value: AuthConfig): ValueProvider => ({
   provide: AUTH_CONFIG,
-  useValue: value,
-});
-
-export const POLICY_CONFIG = new InjectionToken<PolicyConfig>(
-  'ci-policy.config'
-);
-
-export const getPolicyConfigProvider = (
-  value: typeof POLICY_CONFIG
-): ValueProvider => ({
-  provide: POLICY_CONFIG,
   useValue: value,
 });
