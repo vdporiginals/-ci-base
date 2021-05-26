a872f11a42ca03ad51e81a4fb440b9858a130eb9
Duy Phuong <phuong.vd@consultindochina.com>
2021-05-26 08:53:33 +0700

    add public

--- projects/consult-indochina/common/src/public-api.ts
+++ projects/consult-indochina/common/src/public-api.ts
@@ -7,3 +7,7 @@ export * from './lib/config/common-config.interface';
 export * from './lib/config/common.config';
 export * from './lib/ui/ci-table/ci-table.component';
 export * from './lib/ui/ci-table/ci-table.model';
+export * from './lib/utils/unicode-slug';
+export * from './lib/services/s3-file.service';
+export * from './base/base-ci-table';
+export * from './base/base-upload-component';
