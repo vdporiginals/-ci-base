6bfa36810bbb6d235e0513ac1e0788d6cb4ae887
Duy Phuong <phuong.vd@consultindochina.com>
2021-05-28 16:16:11 +0700

    update common

--- projects/consult-indochina/common/src/base/base-upload-component.ts
+++ projects/consult-indochina/common/src/base/base-upload-component.ts
@@ -1,6 +1,6 @@
 import { Directive } from '@angular/core';
-import { Observable } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
+import { from, merge, Observable, of } from 'rxjs';
+import { concatMap, switchMap, tap } from 'rxjs/operators';
 import { S3FileService } from '../lib/services/s3-file.service';
 
 @Directive()
@@ -8,6 +8,7 @@ export abstract class BaseUploadComponent {
   imageLinkUpload!: string;
   fileType!: string[];
   fileName!: string;
+
   constructor(public s3Service: S3FileService) {}
   selectImage(file: File[]): Observable<any> {
     this.fileType = file[0].name.split('.');
@@ -27,5 +28,33 @@ export abstract class BaseUploadComponent {
           return this.s3Service.uploadImage(res.payload, file[0], file[0].type);
         })
       );
+  }
+
+  multipleUpload(file: File[]): Observable<any> {
+    let fileLinkList: string[] = [];
+    return from(file).pipe(
+      switchMap((f) => {
+        let fileName = f.name.substr(0, file[0].name.indexOf('.'));
+        let typeFile = f.name.split('.');
+        return this.s3Service
+          .generatePresigned({
+            contentType: f.type,
+            name: fileName,
+            type: typeFile[typeFile.length - 1],
+          })
+          .pipe(
+            concatMap((res: any) => {
+              // fileLink = ;
+              fileLinkList.push(
+                res.payload.substr(0, res.payload.indexOf('?'))
+              );
+              return merge(
+                of(fileLinkList),
+                this.s3Service.uploadImage(res.payload, f, f.type)
+              );
+            })
+          );
+      })
+    );
   }
 }
